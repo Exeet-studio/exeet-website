1d88372a305fd689d47badb7260a0ab2f96639ff
Exeet-studio <exeet4711@gmail.com>
2026-07-27 20:14:40 +0800

    Update data.js

--- data.js
+++ data.js
@@ -472,7 +472,7 @@ const TEAM = [
               'assets/team/張恩ㄐㄩ/甲蟲超人/IMG_0323.jpg',
               'assets/team/張恩ㄐㄩ/甲蟲超人/IMG_0324.jpg',
               'assets/team/張恩ㄐㄩ/甲蟲超人/IMG_0325.jpg',
-              'assets/team/張恩ㄐㄩ/甲蟲超人/IMG_0326.jpg'
+              'assets/team/張恩ㄐㄩ/甲蟲超人/IMG_0326.jpg',
               
             ]
           }
@@ -514,50 +514,7 @@ const TEAM = [
               'assets/team/張恩ㄐㄩ/平行線/HAND X Zhen Li【平行線】(Official Music Video ) 1-57 screenshot.png',
               'assets/team/張恩ㄐㄩ/平行線/HAND X Zhen Li【平行線】(Official Music Video ) 2-6 screenshot.png'
             ]
-          },
-
-          {
-            slug: 'superman',
-            title: '甲蟲超人',
-            image: 'assets/team/張恩ㄐㄩ/destroy me/VS--YouTube-53DestroyMe2-YouTube-0’10”.jpg',
-            image: 'assets/team/張恩ㄐㄩ/甲蟲超人/IMG_0319.jpg',
-            year: '2023',
-            client: '台灣藝術大學電影系畢業製作',
-
-            media: {
-              type: 'video',
-              type: 'image',
-              url: 'assets/team/張恩ㄐㄩ/destroy me/VS--YouTube-53DestroyMe2-YouTube-0’10”.jpg'
-              url: 'assets/team/張恩ㄐㄩ/甲蟲超人/IMG_0319.jpg'
-            },
-
-            description:
-              '',
-              '台灣藝術大學電影系畢業製作',
-
-            credits: [
-              'Director｜劉昱均',
-              'Dop｜林立軒',
-              'Gaffer｜張恩郡',
-            ],
-
-            gallery: [
-              'assets/team/張恩ㄐㄩ/destroy me/VS--YouTube-53DestroyMe2-YouTube-0’47”.jpg',
-              'assets/team/張恩ㄐㄩ/destroy me/VS--YouTube-53DestroyMe2-YouTube-1’33”.jpg',
-              'assets/team/張恩ㄐㄩ/destroy me/VS--YouTube-53DestroyMe2-YouTube-1’37”.jpg',
-              'assets/team/張恩ㄐㄩ/destroy me/VS--YouTube-53DestroyMe2-YouTube-1’41”.jpg',
-              'assets/team/張恩ㄐㄩ/destroy me/VS--YouTube-53DestroyMe2-YouTube-1’44”.jpg'
-             'assets/team/張恩ㄐㄩ/甲蟲超人/IMG_0317.jpg',
-              'assets/team/張恩ㄐㄩ/甲蟲超人/IMG_0318.jpg',
-              'assets/team/張恩ㄐㄩ/甲蟲超人/IMG_0320.jpg',
-              'assets/team/張恩ㄐㄩ/甲蟲超人/IMG_0321.jpg',
-              'assets/team/張恩ㄐㄩ/甲蟲超人/IMG_0322.jpg',
-              'assets/team/張恩ㄐㄩ/甲蟲超人/IMG_0323.jpg',
-              'assets/team/張恩ㄐㄩ/甲蟲超人/IMG_0324.jpg',
-              'assets/team/張恩ㄐㄩ/甲蟲超人/IMG_0325.jpg',
-              'assets/team/張恩ㄐㄩ/甲蟲超人/IMG_0326.jpg'
-            ]
-          },
+          }
 
         ]
       },
